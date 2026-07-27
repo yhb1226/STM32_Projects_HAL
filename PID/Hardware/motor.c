41a11248eb0a67625af4785fe971be6dd69841b2
yhb1226 <3537311140@qq.com>
2026-07-27 22:45:50 +0800

    测试

--- PID/Hardware/motor.c
+++ PID/Hardware/motor.c
@@ -9,7 +9,7 @@ void Motor_SetPWM(uint8_t n, int8_t PWM)
 	{
 		if (PWM >= 0)							//如果设置正转的PWM
 		{
-            HAL_GPIO_WritePin(MOTOR_LEFT1_GPIO_Port, MOTOR_LEFT1_Pin, GPIO_PIN_SET);//PB12置高电平
+            HAL_GPIO_WritePin(MOTOR_LEFT1_GPIO_Port, MOTOR_LEFT1_Pin, GPIO_PIN_SET); //PB12置高电平
             HAL_GPIO_WritePin(MOTOR_LEFT2_GPIO_Port, MOTOR_LEFT2_Pin, GPIO_PIN_RESET);//PB13置低电平
 			__HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, PWM);//设置PWM占空比
 		}
